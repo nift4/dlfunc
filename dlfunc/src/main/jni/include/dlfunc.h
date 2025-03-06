bca1bc050b8d596becfcf44027938eb0fa025957
nift4 <nift4@protonmail.com>
2025-03-06 13:44:57 +0100

    fix missing extern C

--- dlfunc/src/main/jni/include/dlfunc.h
+++ dlfunc/src/main/jni/include/dlfunc.h
@@ -3,13 +3,21 @@
 
 #include <jni.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // setup the env, must be called before the other functions
-int dlfunc_init(JNIEnv* env);
+int dlfunc_init(JNIEnv *env);
 
 // call dlopen(filename, flags)
 void *dlfunc_dlopen(JNIEnv *env, const char *filename, int flags);
 
 // call dlsym(handle, symbol)
 void *dlfunc_dlsym(JNIEnv *env, void *handle, const char *symbol);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //YAHFA_DLFUNC_H
